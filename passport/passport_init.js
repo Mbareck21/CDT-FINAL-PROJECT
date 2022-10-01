3016b5b3090142f8675cf8bf1ed3bc486a117bc6
Mbareck21 <missara.lamine@gmail.com>
2022-10-01 16:10:50 -0500

    sign up and login pages working

--- passport/passport_init.js
+++ passport/passport_init.js
@@ -1,18 +1,18 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/User");
-
+const bcrypt = require("bcryptjs");
 const passport_init = () => {
   passport.use(
-    new LocalStrategy((username, password, done) => {
-      User.findOne({ username: username }, (err, user) => {
+    new LocalStrategy({ usernameField: 'email' },(email, password, done) => {
+      User.findOne({ email: email }, (err, user) => {
         if (err) {
           return done(err);
         }
         if (!user) {
           return done(null, false, { message: "Incorrect credentials." });
         }
-        user.comparePassword(password, (err, result) => {
+        bcrypt.compare(password, user.password, (err, result) => {
           if (result) {
             return done(null, user);
           } else {
